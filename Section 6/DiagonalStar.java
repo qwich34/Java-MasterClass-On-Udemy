e5434a9950d583ce414522c519c7a2d19943d946
Ilmir Kireev <55609661+qwich34@users.noreply.github.com>
2023-10-26 23:24:39 -0400

    Add files via upload

---
+++ Section 6/DiagonalStar.java
@@ -0,0 +1,21 @@
+public class DiagonalStar {
+    // write your code here
+      public static void printSquareStar(int number){
+        if(number < 5){
+            System.out.println("Invalid Value");
+        } else{
+        for(int i = 0; i < number; i++) {
+            for (int j = 0; j < number; j++) {
+                if (i == 0 || i == number - 1 || j == 0 || j == number - 1 || j == i || j == number - 1 - i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+        }
+
+    }
+
+}
