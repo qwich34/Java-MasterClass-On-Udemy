833104cd7e9c4676437bc479d4494591f4e402a0
Ilmir Kireev <55609661+qwich34@users.noreply.github.com>
2023-10-26 23:23:42 -0400

    Add files via upload

---
+++ Section 6/LargestPrime.java
@@ -0,0 +1,17 @@
+public class LargestPrime {
+    // write your code here
+    public static int getLargestPrime(int number){
+        if(number < 2){
+            return -1;
+        }
+
+        for(int i = 2; i < number; i++){
+            if((number % i) == 0){
+                number /= i;
+                i--;
+            }
+        }
+        return number;
+    }
+    
+}
