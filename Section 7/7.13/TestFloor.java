84cd72995cf8e00d214e2dbac33ce539d4c3b739
Ilmir Kireev <55609661+qwich34@users.noreply.github.com>
2023-10-28 20:35:19 -0400

    Delete Section 7 directory

--- Section 7/7.13/TestFloor.java
+++
@@ -1,9 +0,0 @@
-public class TestFloor {
-    public static void main(String[] args) {
-        Floor floor = new Floor(12,13);
-        Carpet carpet = new Carpet(27);
-        Calculator calculator = new Calculator(floor,carpet);
-
-        System.out.println("$" + calculator.getTotalCost());
-    }
-}
