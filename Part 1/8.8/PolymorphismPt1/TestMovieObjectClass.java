912eaed8efd4653a87c6bdcfc52aa0236b0b1de1
Ilmir Kireev <55609661+qwich34@users.noreply.github.com>
2023-10-30 19:54:58 -0400

    Delete Part 1 directory

--- Part 1/8.8/PolymorphismPt1/TestMovieObjectClass.java
+++
@@ -1,8 +0,0 @@
-package PolymorphismPt1;
-
-public class TestMovieObjectClass {
-    public static void main(String[] args) {
-        Movie theMovie = new Adventure("Star Wars");
-        theMovie.watchMovie();
-    }
-}
